051c44eec30574963e5879fdf3d56ff67138bde9
ld <ldshuang@gmail.com>
2018-05-20 16:14:45 +0800

    go-chi/chi package

--- main.go
+++ main.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-  "fmt"
-  "log"
   "net/http"
+  "github.com/go-chi/chi"
+  "github.com/go-chi/chi/middleware"
 )
 
 func main() {
-  http.HandleFunc("/", handle)
-  http.HandleFunc("/_ah/health", healthCheckHandler)
-  log.Print("Listening on port 8080")
-  log.Fatal(http.ListenAndServe(":8080", nil))
-}
+  r := chi.NewRouter()
+  r.Use(middleware.Logger)
+  r.Use(middleware.Recoverer)
 
-func handle(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path != "/" {
-    http.NotFound(w, r)
-    return
-  }
-  fmt.Fprint(w, "Hello world!")
-}
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "ok")
+  r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("welcome"))
+  })
+  r.Get("/_ah/health", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("ok"))
+  })
+  http.ListenAndServe(":3000", r)
 }
